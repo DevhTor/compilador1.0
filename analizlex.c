7793d98b8569eaacaf8bea771d5d35c1f52be99f
Hector Aristy Castillo Mateo <hectoristy@hotmail.com>
2022-03-17 13:54:20 -0400

    formateado

--- analizlex.c
+++ analizlex.c
@@ -1,49 +1,58 @@
-//analizlex.c
+// analizlex.c
 
 #include "global.h"
 //#include "error.c"
 
 char buflex[TAMBUFF];
 int numlinea = 1;
-int valcomplex= NINGUNO;
+int valcomplex = NINGUNO;
 
-int analex()
-{ 
-	int t;
-	while(1) {
+int analex() // analizador lexico
+{
+	int t;	  // tipo de lexema
+	while (1) // bucle infinito
+	{
 		t = getchar();
-		if (t == ' '|| t == '\t')
-			;				
-		else if (t== '\n')
-			numlinea = numlinea + 1;
-			else if (isdigit(t)) {
-					ungetc(t, stdin);
-					scanf("%d", &valcomplex);
-					return NUM;
-				}
-				else if(isalpha(t)) {
-					int p, b = 0;
-					while (isalnum (t)) {
-						buflex[b] = t;
-						t = getchar();
-						b = b + 1;
-						if (b >= TAMBUFF)
-							error("ERROR DE COMPILADOR");
-					}
-					buflex[b] = FDC;
-					if (t != EOF)
-						ungetc(t, stdin);
-					p = busca(buflex);
-					if (p == 0)
-						p = inserta(buflex, ID);
-					valcomplex = p;
-					return tablasimb[p].complex;
-				}
-				else if (t == EOF)
-					return FIN;
-				else {
-					valcomplex = NINGUNO;
-					return t;
-				}
+		// lee caracter
+		if (t == ' ' || t == '\t') // si es espacio o tabulador
+			;					   // no hace nada
+
+		else if (t == '\n')			 // si es salto de linea
+			numlinea = numlinea + 1; // incrementa numero de linea
+
+		else if (isdigit(t)) // si es digito
+		{
+			ungetc(t, stdin);		  // devuelve caracter a la entrada
+			scanf("%d", &valcomplex); // lee numero entero
+			return NUM;				  // retorna NUM
+		}
+		else if (isalpha(t))
+		{
+
+			int p, b = 0;
+			while (isalnum(t))
+			{
+				buflex[b] = t;
+				t = getchar();
+				b = b + 1;
+				if (b >= TAMBUFF)
+					error("ERROR DE COMPILADOR");
+			}
+			buflex[b] = FDC;
+			if (t != EOF)
+				ungetc(t, stdin);
+			p = busca(buflex);
+			if (p == 0)
+				p = inserta(buflex, ID);
+			valcomplex = p;
+			return tablasimb[p].complex;
+		}
+		else if (t == EOF)
+			return FIN;
+		else
+		{
+			valcomplex = NINGUNO;
+			return t;
+		}
 	}
 }
