5832a1839e40d1d701740e590fcbb9e71451b7b9
hectoristy <34715623+hectoristy@users.noreply.github.com>
2022-03-16 21:26:25 -0400

    analizlex funcionando

--- simbolos.c
+++ simbolos.c
@@ -44,54 +44,7 @@ int clex;
 
 
 
-//analizlex.c
 
-//#include "error.c"
-
-char buflex[TAMBUFF];
-int numlinea = 1;
-int valcomplex= NINGUNO;
-
-int analex()
-{ 
-	int t;
-	while(1) {
-		t = getchar();
-		if (t == ' '|| t == '\t')
-			;				
-		else if (t== '\n')
-			numlinea = numlinea + 1;
-			else if (isdigit(t)) {
-					ungetc(t, stdin);
-					scanf("%d", &valcomplex);
-					return NUM;
-				}
-				else if(isalpha(t)) {
-					int p, b = 0;
-					while (isalnum (t)) {
-						buflex[b] = t;
-						t = getchar();
-						b = b + 1;
-						if (b >= TAMBUFF)
-							error("ERROR DE COMPILADOR");
-					}
-					buflex[b] = FDC;
-					if (t != EOF)
-						ungetc(t, stdin);
-					p = busca(buflex);
-					if (p == 0)
-						p = inserta(buflex, ID);
-					valcomplex = p;
-					return tablasimb[p].complex;
-				}
-				else if (t == EOF)
-					return FIN;
-				else {
-					valcomplex = NINGUNO;
-					return t;
-				}
-	}
-}
 
 
 
@@ -100,6 +53,7 @@ int analex()
 
 #include "emisor.c"
 //#include "error.c"
+#include "analizlex.c"
 
 void analsint() {
 	
